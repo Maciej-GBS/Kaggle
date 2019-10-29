c12c03810bb6a38dccf6633d57bfa7b07fac6e2d
GummyBearStudio <keyboardwarriror@gmail.com>
2019-10-29 16:51:18 +0100

    Separate features for classes

--- core.py
+++ core.py
@@ -7,12 +7,15 @@ def load_meta():
 
 def load(path, meta, cols=['Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket']):
     features = pd.read_csv(path, usecols=cols)
-    for nom in ['Pclass','Name','Age','SibSp','Parch','Ticket']:
+    for nom in ['Name','Age','SibSp','Parch','Ticket']:
         if nom in list(features.columns):
             nparr = np.array(features[nom]) - meta[nom]
             features.loc[:, nom] = nparr / max(np.abs(nparr))
+    if 'Pclass' in list(features.columns):
+        classe = pd.DataFrame({'1st':np.where(features['Pclass']==1,1,0), '2nd':np.where(features['Pclass']==2,1,0), '3rd':np.where(features['Pclass']==3,1,0)})
+        features = features.drop('Pclass', axis=1)
+        features = pd.concat([features, classe], axis=1)
     return features
-    # Pclass should be divided to 3 features determining each class
 
 def load_dataset(meta):
     data = load('dataset/train.csv', meta)
@@ -26,29 +29,30 @@ def load_test(meta):
 
 def main():
     tf.keras.backend.clear_session()
-    inputs = tf.keras.Input(shape=(7,))
+    inputs = tf.keras.Input(shape=(9,))
     x = tf.keras.layers.Dense(64)(inputs)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.LeakyReLU(alpha=0.1)(x)
     outputs = tf.keras.layers.Dense(2, activation='softmax')(x)
     model = tf.keras.Model(inputs=inputs, outputs=outputs, name='titanic_model')
     model.summary()
-    model.compile(optimizer=tf.keras.optimizers.SGD(), loss='mse', metrics=['accuracy'])
+    model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.001), loss='mse', metrics=['accuracy'])
     metadata = load_meta()
     x_train, y_train = load_dataset(metadata)
     print(x_train)
-    history = model.fit(x=x_train, y=y_train, batch_size=32, epochs=10, validation_split=0.1)
+    history = model.fit(x=x_train, y=y_train, batch_size=32, epochs=100, validation_split=0.1)
     model.save('models/pre.h5')
     print("="*50)
-    print(history.history)
+    print(history.history['acc'])
     o = load_test(metadata)
     labels = model.predict(x=o)
     labels = pd.DataFrame(labels, columns=['S', 'M'])
     labels = pd.Series(np.where(labels['S'] > labels['M'], 1, 0))
     result = load('dataset/test_id.csv', metadata, ['PassengerId'])
     result = pd.concat([result, labels], axis=1)
+    result.rename(columns={0:'Survived'}, inplace=True)
     print(result)
-    result.to_csv('output.csv')
+    result.to_csv('output.csv', index=False)
 
 if __name__=='__main__':
     main()
